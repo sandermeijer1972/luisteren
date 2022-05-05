806290474fc7e153aa9c11173502919b2fd28a02
Sander Meijer <sandertje1972@hotmail.com>
2022-05-05 18:04:15 +0200

    added script for changing the DOM

---
+++ scripts/script.js
@@ -0,0 +1,93 @@
+const main = document.getElementById('main');
+const luisterNav = document.getElementById('luisternav');
+const luisterHome = document.getElementById('luisterhome');
+const accessNav = document.getElementById('accessnav');
+const accessHome = document.getElementById('accesshome');
+const overNav = document.getElementById('overnav');
+const overHome = document.getElementById('overhome');
+const recensiesNav = document.getElementById('recensiesnav');
+const contactNav = document.getElementById('contactnav');
+
+const clearMenu = () => {
+    main.innerHTML = '';
+    luisterNav.classList.remove('active');
+    accessNav.classList.remove('active');
+    overNav.classList.remove('active');
+    recensiesNav.classList.remove('active');
+    contactNav.classList.remove('active');
+};
+
+const addLuisterkindToDOM = () => {    
+    clearMenu();
+    luisterNav.classList.add('active');
+    const newArticle = document.createElement('article');
+    newArticle.classList.add('listening');
+    const newH1 = document.createElement('h1');
+    newH1.innerText = listenText[0];
+    newArticle.appendChild(newH1);
+    const firstSection = document.createElement('section');
+    firstSection.innerText = listenText[1];
+    newArticle.appendChild(firstSection);
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('logo');
+    const newImg = document.createElement('img');
+    newImg.src = "./images/Logo-Luisterkind-werkers[3344].png";
+    newDiv.appendChild(newImg);
+    newArticle.appendChild(newDiv);
+    for (i=2; i<5; i++) {
+        const newSection = document.createElement('section');
+        newSection.innerText = listenText[i];
+        newArticle.appendChild(newSection);
+    };
+    main.appendChild(newArticle);
+};
+
+luisterNav.addEventListener('click', addLuisterkindToDOM);
+luisterHome.addEventListener('click', addLuisterkindToDOM);
+
+
+const addAccessBarsToDOM = () => {
+    clearMenu();
+    accessNav.classList.add('active');
+    const newArticle = document.createElement('article');
+    newArticle.classList.add('bars');
+    const newH1 = document.createElement('h1');
+    newH1.innerText = accessText[0];
+    newArticle.appendChild(newH1);
+    const firstSection = document.createElement('section');
+    firstSection.innerText = accessText[1];
+    newArticle.appendChild(firstSection);
+    main.appendChild(newArticle);
+};
+
+accessNav.addEventListener('click', addAccessBarsToDOM);
+accessHome.addEventListener('click', addAccessBarsToDOM);
+
+
+const addOverMijToDOM = () => {
+    clearMenu();
+    overNav.classList.add('active');
+    const newArticle = document.createElement('article');
+    newArticle.classList.add('info');
+    const newH1 = document.createElement('h1');
+    newH1.innerText = infoText[0];
+    newArticle.appendChild(newH1);
+    const firstSection = document.createElement('section');
+    firstSection.innerText = infoText[1];
+    newArticle.appendChild(firstSection);
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('photo');
+    const newImg = document.createElement('img');
+    newImg.src = "./images/marieke-info.jpg";
+    newDiv.appendChild(newImg);
+    newArticle.appendChild(newDiv);
+    for (i=2; i<4; i++) {
+        const newSection = document.createElement('section');
+        newSection.innerText = infoText[i];
+        newArticle.appendChild(newSection);
+    };
+    main.appendChild(newArticle);
+};
+
+overNav.addEventListener('click', addOverMijToDOM);
+overHome.addEventListener('click', addOverMijToDOM);
---
+++ scripts/texts.js
@@ -0,0 +1,19 @@
+const listenText = [
+    'Wat is een Luisterkind Afstemming?',
+    'Een luisterkind afstemming is een gesprek met een ziel op afstand. Je zult denken dat dit alleen voor kinderen is, maar niets is minder waar. Namelijk op alles wat energie heeft kan worden afgestemd.',
+    'Denk daarbij aan je huisdieren die onrustig zijn, of een dierbare die je heel graag nog wat wilt vragen omdat hij of zij plotseling overleden is. Of wanneer je een vraag hebt over jezelf kun je ook gewoon bij mij terecht.',
+    'Wat ik doe? Ik maak contact met jou, met jouw ziel. Of met de persoon of huisdier voor wie je de afstemming aanvraagt. Alles wat ik voel, weet of zie schrijf ik op. Daar komt een verhaal uit en dat krijg jij op papier. Binnen 14 dagen heb jij een mooi document in je mailbox met alle antwoorden op jouw vragen. Misschien zul je niet alles direct herkennen, maar lees het na een aantal maanden nog eens. En wellicht zul je de verandering ineens wel zien!',
+    'Dàt is luisterkind!',
+];
+
+const accessText = [
+    'Wat is Access Bars?',
+    'Hier komt het verhaal over Access Bars',
+];
+
+const infoText = [
+    'Wie ben ik?',
+    'Ik ben Marieke Onderwater, 37 jaar en moeder van 2 prachtige kinderen. Een koningsgezin, een jongen en meid van 3 en 5 jaar oud. Daarnaast werk ik nog 2 dagen in de kinderopvang.',
+    'Toen ik hoorde van Luisterkind, heb ik zelf direct een aantal keer een afstemming aangevraagd voor mijn eigen kinderen. Direct zag ik verschil, aan de energie van mijn kinderen en antwoorden van mijn vragen werden opgelost. Dit vond ik zo iets bijzonders! Na enige tijd hoorde ik van de opleiding Luisterkind, en dacht meteen: “Ja, dit ben ik en dit wil ik worden!”',
+    'Altijd al help ik graag anderen, geef ik tips vanuit mijn eigen intuïtie. Het zit in de aard van het beestje zeggen ze dan. Nu ik deze opleiding heb kunnen afronden kan ik er mijn werk van maken. En ik ben dankbaar en blij dat ik meer moeders en vaders mag gaan helpen. Want ik weet als geen ander dat het niet altijd makkelijk is, het opvoeden van je eigen kinderen.',
+];
